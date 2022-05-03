7abe7bad5bbb8ea2d7079940ba25f7273e8dde03
lorenzo-pachioli <lorenzopachioli@gmail.com>
2022-05-03 10:37:07 -0300

    first

---
+++ index.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const cors = require('cors');
+const logger = require('./logger.js');
+
+const app = express();
+
+app.use(express.json())
+app.use(cors())
+
+app.use(logger)
+
+let notes = [
+    {
+        id:1, 
+        content:"Primera nota con node"
+    },
+    {
+        id:2, 
+        content:"Segunda nota con node"
+    }, 
+    {
+        id:3, 
+        content:"Tercera nota con node"
+    }
+]
+
+app.get('/', (request, response)=> {
+    response.send('<h1>Hellow world</h1>')
+})
+
+app.get('/notes', (request, response)=> {
+    response.json(notes)
+})
+
+app.get('/notes/:id', (request, response)=> {
+    const id = request.params.id;
+    const resp = notes.find((note=> note.id === Number(id)))
+    if(resp){
+        response.json(resp);
+    }else{
+        response.status(404).end(' 404 note not found')
+    }
+    
+})
+
+app.delete('/notes/:id', (request, response)=> {
+    const id = Number(request.params.id);
+    notes = notes.filter((note=> note.id !== id))
+    response.json(notes)
+})
+
+app.post('/notes/:post', (request, response)=> {
+    const note = request.params.post;
+
+    if(!note){
+        response.status(400).end("content is missing");
+    }
+
+    const ids = notes.map((note) => note.id)
+    const maxId = Math.max(...ids) 
+    const newNote = {
+        id: maxId + 1,
+        content: note
+    }
+    notes = [...notes, newNote]
+    response.json(notes);
+})
+
+
+const Port = 3001
+app.listen(Port, ()=> {
+    console.log(`Server running on port ${Port}`)
+});
---
+++ logger.js
@@ -0,0 +1,10 @@
+const logger = (request, response, next) => {
+    
+    console.log(request.method)
+    console.log(request.body)
+    console.log(request.path)
+    console.log("----------")
+    next()
+} 
+
+module.exports = logger;
