f472cea3da9871adef4f35cadd0fc502be92f8a6
lorenzo-pachioli <lorenzopachioli@gmail.com>
2022-05-26 17:05:55 -0300

    uri

--- mongoDB.js
+++ mongoDB.js
@@ -5,7 +5,7 @@ const password = 'UMegFECG3rLW3AU9';
 const uri = process.env.MONGODB_URI;
 const db = `mongodb+srv://Lorenzo-Pachioli:${password}@cluster0.afsld.mongodb.net/?retryWrites=true&w=majority`;
 
-mongoose.connect(uri ? (uri):(db))
+mongoose.connect(uri)
 .then(() => console.log('Connected'))
 .catch(error => console.error(error))
 
