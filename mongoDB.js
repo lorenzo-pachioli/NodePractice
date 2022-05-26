8dfdc94b4d158bb06ab507deac8ba7896865ffbb
lorenzo-pachioli <lorenzopachioli@gmail.com>
2022-05-26 15:25:06 -0300

    acces change

--- mongoDB.js
+++ mongoDB.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 /* const password = require('./password.js'); */
-/* const password = 'UMegFECG3rLW3AU9'; */
+const password = 'UMegFECG3rLW3AU9';
 
 const uri = process.env.MONGODB_URI;
-/* const db = `mongodb+srv://Lorenzo-Pachioli:${password}@cluster0.afsld.mongodb.net/?retryWrites=true&w=majority`; */
+const db = `mongodb+srv://Lorenzo-Pachioli:${password}@cluster0.afsld.mongodb.net/?retryWrites=true&w=majority`;
 
-mongoose.connect(uri)
+mongoose.connect(uri ? (uri):(db))
 .then(() => console.log('Connected'))
 .catch(error => console.error(error))
 
